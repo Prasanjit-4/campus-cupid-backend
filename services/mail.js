671eac905e173ddc96f1c4574f53695634f8674a
Prasanjit-4 <prasanjitpanda8@gmail.com>
2024-05-01 18:34:11 +0530

    magic link

--- services/mail.js
+++ services/mail.js
@@ -20,7 +20,7 @@ async function sendMail(recipient, token) {
         to: recipient, // list of receivers
         subject: "Hello ✔", // Subject line
         text: `Token: ${token}`, // plain text body with token
-        html: `<b>Token: ${token}</b>`, // html body with token
+        html: `<b><a href="www.campuscupid.social/login/${token}"></b>`, // html body with token
     });
 
     return info.messageId;
